3101e7605a50738de036c2ad5b17832f05c7aac7
Mohammad Badangiz <mbz3a1998@gmail.com>
2024-02-27 01:32:33 +0330

    BPS-left side cards is started

--- app/BestProductSeller/BPSleft.tsx
+++ app/BestProductSeller/BPSleft.tsx
@@ -1,13 +1,60 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 
 export function BPSleft() {
   return (
     <Grid
       item
-      bgcolor={"green"}
       xs={12}
       md={9}
       height={{ lg: "max-content" }}
-    ></Grid>
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+        gap: "20px",
+      }}
+    >
+      <ProductsCards />
+      <ProductsCards />
+      <ProductsCards />
+    </Grid>
+  );
+}
+
+function ProductsCards() {
+  return (
+    <Box
+      width={"270px"}
+      height={"400px"}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "end",
+      }}
+    >
+      <Box
+        bgcolor={"#C1D0E4"}
+        width={"100%"}
+        height={"83%"}
+        sx={{
+          borderRadius: "40px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "end",
+          boxShadow: "0px 4px 10px 2px #DDDADA40",
+        }}
+      >
+        <Box>1</Box>
+        <Box
+          height={"55%"}
+          bgcolor={"#fff"}
+          sx={{
+            borderRadius: "inherit",
+          }}
+        >
+          2
+        </Box>
+      </Box>
+    </Box>
   );
 }
